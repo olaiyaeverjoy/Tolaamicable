7d5ea3a54f9ea465456a18aca105a2a95b00c879
olaiya everjoy <olaiyaeverjoy02@gmail.com>
2026-05-06 18:37:11 +0100

    playingaround

--- src/views/Homeview.vue
+++ src/views/Homeview.vue
@@ -1,6 +1,7 @@
 <script setup>
 // import Navbar from "../components/Navbar.vue";
 import Footer from "../components/Footer.vue";
+import logo from "../assets/logo/logo.png";
 </script>
 
 <template>
